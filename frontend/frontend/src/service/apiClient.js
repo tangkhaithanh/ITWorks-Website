699ae9d1474c40996562970c5864bbf18f21a12b
huynhsown <sondanghuynh@gmail.com>
2026-01-13 23:40:37 +0700

    feat: Jenkins FE 2

--- frontend/frontend/src/service/apiClient.js
+++ frontend/frontend/src/service/apiClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://itworks.dpdns.org",
+  baseURL: import.meta.env.VITE_API_URL || "https://itworks.dpdns.org/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
