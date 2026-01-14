4324c599156f004ab1bc8910061d71cc73894587
huynhsown <sondanghuynh@gmail.com>
2026-01-15 03:17:48 +0700

    Add credentials for ES

--- backend/src/modules/elasticsearch/elasticsearch.module.ts
+++ backend/src/modules/elasticsearch/elasticsearch.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { Client } from '@elastic/elasticsearch';
 import { ElasticsearchCompanyService } from './company.elasticsearch.service';
 import { ElasticsearchJobService } from './job.elasticsearch.service';
+
 @Global()
 @Module({
   providers: [
@@ -13,16 +14,25 @@ import { ElasticsearchJobService } from './job.elasticsearch.service';
         const node =
           configService.get<string>('ELASTICSEARCH_NODE') ||
           'http://localhost:9200';
+
+        const username =
+          configService.get<string>('ELASTICSEARCH_USERNAME') || 'elastic';
+
+        const password =
+          configService.get<string>('ELASTICSEARCH_PASSWORD') || '';
+
         console.log('✅ Elasticsearch node:', node);
+        console.log('✅ Elasticsearch user:', username);
 
         return new Client({
           node,
+          auth: {
+            username,
+            password,
+          },
           maxRetries: 1,
           requestTimeout: 5000,
           sniffOnStart: false,
-          sniffInterval: false,
-          sniffOnConnectionFault: false,
-          name: 'local-node',
         });
       },
     },
