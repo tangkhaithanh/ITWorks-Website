267b7a922d75feba498c482727f54ebf8ebbb479
huynhsown <sondanghuynh@gmail.com>
2026-05-10 10:50:34 +0700

    fix: add controller

--- backend/src/modules/notifications/notifications.module.ts
+++ backend/src/modules/notifications/notifications.module.ts
@@ -3,10 +3,11 @@ import { NotificationsGateway } from './notifications.gateway';
 import { WsJwtGuard } from '@/common/guards/ws-jwt.guard';
 import { WsAuthModule } from '@/common/services/ws/ws-auth.module';
 import { NotificationsService } from '@/modules/notifications/notifications.service';
+import { NotificationsController } from './notifications.controller';
 @Module({
   imports: [WsAuthModule],
   providers: [NotificationsGateway, WsJwtGuard , NotificationsService],
   exports: [NotificationsGateway, NotificationsService],
-  controllers: [],
+  controllers: [NotificationsController],
 })
 export class NotificationsModule {}
